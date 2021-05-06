2c8127daceff98af7251588937ec94f5bf52630e
Konstantin <ferr311@mail.ru>
2021-05-06 14:22:53 +0300

    add PokeList

---
+++ app/src/main/java/com/example/pokemons/data/models/PokemonListEntry.kt
@@ -0,0 +1,7 @@
+package com.example.pokemons.data.models
+
+data class PokemonListEntry(
+    val name:String,
+    val imageUrl:String,
+    val number:Int
+)
--- app/src/main/java/com/example/pokemons/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/example/pokemons/pokemonlist/PokemonListScreen.kt
@@ -2,26 +2,42 @@ package com.example.pokemons.pokemonlist
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltNavGraphViewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.navigate
+import coil.request.ImageRequest
 import com.example.pokemons.R
+import com.example.pokemons.data.models.PokemonListEntry
+import com.example.pokemons.ui.theme.Roboto
+import com.example.pokemons.ui.theme.RobotoCondensed
+import com.google.accompanist.coil.CoilImage
 
 
 @Composable
@@ -50,6 +66,8 @@ fun PokemonListScreen(
             ) {
                 //todo implement in viewmodel
             }
+            Spacer(modifier = Modifier.height(16.dp))
+            PokemonList(navController = navController)
         }
     }
 }
@@ -90,6 +108,148 @@ fun SearchBar(
                 color = Color.LightGray,
                 modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp)
             )
+        }
+    }
+}
+
+@Composable
+fun PokemonList(
+    navController: NavController,
+    viewModel: PokemonListViewModel = hiltNavGraphViewModel()
+) {
+    val pokemonList by remember { viewModel.pokemonList }
+    val endReached by remember { viewModel.endReached }
+    val isLoading by remember { viewModel.isLoading }
+    val loadError by remember { viewModel.loadError }
+
+    LazyColumn(contentPadding = PaddingValues(16.dp)) {
+        val itemCount = if (pokemonList.size % 2 == 0) {
+            pokemonList.size / 2
+        } else {
+            pokemonList.size / 2 + 1
+        }
+        items(itemCount) {
+            if (it >= itemCount - 1 && !endReached) {
+                viewModel.loadPokemonPaginated()
+            }
+            PokemonRow(rowIndex = it, entries = pokemonList, navController = navController)
+        }
+    }
+    Box(
+        contentAlignment = Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if (isLoading) {
+            CircularProgressIndicator(color = MaterialTheme.colors.primary)
+        }
+        if (loadError.isNotEmpty()) {
+            RetrySection(error = loadError) {
+                viewModel.loadPokemonPaginated()
+            }
+        }
+    }
+}
+
+@Composable
+fun PokemonEntry(
+    entry: PokemonListEntry,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    viewModel: PokemonListViewModel = hiltNavGraphViewModel()
+) {
+    val defaultDominantColor = MaterialTheme.colors.surface
+    var dominantColor by remember { mutableStateOf(defaultDominantColor) }
+    Box(
+        contentAlignment = Center,
+        modifier = modifier
+            .shadow(5.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .aspectRatio(1f)
+            .background(
+                Brush.verticalGradient(
+                    listOf(
+                        dominantColor,
+                        defaultDominantColor
+                    )
+                )
+            )
+            .clickable {
+                navController.navigate("pokemon_detail_screen/${dominantColor.toArgb()}/${entry.name}")
+            }
+    ) {
+        Column {
+            CoilImage(
+                request = ImageRequest.Builder(LocalContext.current)
+                    .data(entry.imageUrl)
+                    .target {
+                        viewModel.calcDominantColor(it) { color ->
+                            dominantColor = color
+                        }
+                    }
+                    .build(),
+                contentDescription = entry.name,
+                fadeIn = true,
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(CenterHorizontally)
+            ) {
+                CircularProgressIndicator(
+                    color = MaterialTheme.colors.primary,
+                    modifier = Modifier.scale(0.5f)
+                )
+            }
+            Text(
+                text = entry.name,
+                fontFamily = RobotoCondensed,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Composable
+fun PokemonRow(
+    rowIndex: Int,
+    entries: List<PokemonListEntry>,
+    navController: NavController
+) {
+    Column {
+        Row {
+            PokemonEntry(
+                entry = entries[rowIndex * 2],
+                navController = navController,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            if (entries.size >= rowIndex * 2 + 2) {
+                PokemonEntry(
+                    entry = entries[rowIndex * 2 + 1],
+                    navController = navController,
+                    modifier = Modifier.weight(1f)
+                )
+            } else {
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@Composable
+fun RetrySection(
+    error: String,
+    onRetry: () -> Unit
+) {
+    Column {
+        Text(text = error, color = Color.Red, fontSize = 18.sp)
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = { onRetry() },
+            modifier = Modifier.align(CenterHorizontally)
+        ) {
+            Text(text = "Retry")
         }
     }
 }
